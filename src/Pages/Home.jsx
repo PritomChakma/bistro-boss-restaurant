a3f908f90cbfc01c39ace00e4310b6368ff19b36
pritom <chakmapritom1@gmail.com>
2024-12-29 02:58:27 +0600

    added a banner with image and title

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Banner from "../Components/Banner";
+import BistroBossSection from "./Home/BistroBossSection";
 import Category from "./Home/Category";
 
 const Home = () => {
@@ -7,6 +8,7 @@ const Home = () => {
     <div>
       <Banner></Banner>
       <Category></Category>
+      <BistroBossSection></BistroBossSection>
     </div>
   );
 };
---
+++ src/Pages/Home/BistroBossSection.jsx
@@ -0,0 +1,25 @@
+import img from "../../assets/home/chef-service.jpg";
+
+const BistroBossSection = () => {
+  return (
+    <div
+      className="relative bg-cover bg-center w-full h-[450px]"
+      style={{ backgroundImage: `url(${img})` }}
+    >
+      <div className="flex items-center justify-center h-full ">
+        <div className="text-center max-w-xs md:max-w-lg p-3 md:p-8 bg-white shadow-lg rounded-md">
+          <h1 className="text-3xl font-bold mb-4">BISTRO BOSS</h1>
+          <p className="text-gray-700">
+            Welcome to Bistro Boss, where flavors come alive and dining becomes
+            an unforgettable experience. Indulge in a culinary journey crafted
+            with passion, precision, and the finest ingredients. From exquisite
+            appetizers to decadent desserts, every dish is a celebration of
+            taste and creativity.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BistroBossSection;
